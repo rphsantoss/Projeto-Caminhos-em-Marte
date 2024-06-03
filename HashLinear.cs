96dd8add49327168e68e536e5372b9303a96c130
Raphael da Silva Santos <raapphaaeell@gmail.com>
2024-06-03 20:22:37 -0300

    implement HashLinear

--- HashLinear.cs
+++ HashLinear.cs
@@ -3,30 +3,100 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace apCaminhosEmMarte
 {
-  public class HashLinear<Tipo> : ITabelaDeHash<Tipo>
-    where Tipo : IRegistro<Tipo>
-  {
-    public List<Tipo> Conteudo()
+    public class HashLinear<Tipo> : ITabelaDeHash<Tipo>
+      where Tipo : IRegistro<Tipo>
     {
-      throw new NotImplementedException();
-    }
+        private Tipo[] tabelaDeHash;
+        private int quantidadeElementos = 0;
+        private int tamanhoPadrao = 131;
 
-    public bool Existe(Tipo item, out int onde)
-    {
-      throw new NotImplementedException();
-    }
+        public Tipo[] Dados { get => tabelaDeHash; }
 
-    public void Inserir(Tipo item)
-    {
-      throw new NotImplementedException();
-    }
+        public HashLinear()
+        {
+            tabelaDeHash = new Tipo[tamanhoPadrao];
+        }
 
-    public bool Remover(Tipo item)
-    {
-      throw new NotImplementedException();
+        private int Hash(string chave)
+        {
+            for (int i = 0; i < tabelaDeHash.Length; i++)
+            {
+                if (tabelaDeHash[i] != null && chave.Equals(tabelaDeHash[i].Chave))
+                {
+                    return i;
+                }
+            }
+
+            long total = 0;
+
+            for (int i = 0; i < chave.Length; i++)
+                total += 37 * total + (char)chave[i];
+
+            total = total % tabelaDeHash.Length;
+
+            if (total < 0)
+                total += tabelaDeHash.Length;
+
+            int posicaoInicial = (int)total;
+            int posicaoAtual = posicaoInicial;
+            int colisoes = 0;
+
+            while (tabelaDeHash[posicaoAtual] != null && (posicaoAtual != posicaoInicial || colisoes == 0))
+            {
+                posicaoAtual++;
+                colisoes++;
+            }
+
+            return (int)total;
+        }
+
+        public List<Tipo> Conteudo()
+        {
+            var saida = new List<Tipo>();
+
+            for (int i = 0; i < tabelaDeHash.Length; i++)
+                if (tabelaDeHash[i] != null)
+                    saida.Add(tabelaDeHash[i]);
+
+            return saida;
+        }
+
+        public bool Existe(Tipo item, out int posicao)
+        {
+            posicao = Hash(item.Chave);
+
+            if (tabelaDeHash[posicao].Equals(item))
+                return true;
+
+
+            return false;
+        }
+
+        public void Inserir(Tipo item)
+        {
+            if (quantidadeElementos < tabelaDeHash.Length)
+            {
+                int valorHash = Hash(item.Chave);
+                tabelaDeHash[valorHash] = item;
+            }
+
+        }
+
+        public bool Remover(Tipo item)
+        {
+            int valorDeHash = Hash(item.Chave);
+
+            if (tabelaDeHash[valorDeHash] != null && tabelaDeHash[valorDeHash].Equals(item))
+            {
+                tabelaDeHash[valorDeHash].Equals(null);
+                return true;
+            }
+            return false;
+        }
+
     }
-  }
 }
